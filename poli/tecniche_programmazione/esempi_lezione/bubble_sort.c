8472e508e525b570661d286776e66f5160d3bbec
marcoce005 <marcoce005@gmail.com>
2025-03-14 18:38:03 +0100

    add in poli: add optimized bubble sort

--- poli/tecniche_programmazione/esempi_lezione/bubble_sort.c
+++ poli/tecniche_programmazione/esempi_lezione/bubble_sort.c
@@ -2,20 +2,40 @@
 
 void print_vet(int len, int v[len]);
 void bubble_sort(int len, int v[len]);
+void optimized_bubble_sort(int len, int v[len]);
 
 int main(void)
 {
-    int v[] = {3, 5, 7, 4, 8, 9, 4, 3, 2, 1};
+    int v[] = {14, 6, 2, 5, 6, 2, 9, 11, 3};
 
     printf("\nBefore:\t");
     print_vet(sizeof(v) / sizeof(int), v);
 
-    bubble_sort(sizeof(v) / sizeof(int), v);
+    // bubble_sort(sizeof(v) / sizeof(int), v);
+    optimized_bubble_sort(sizeof(v) / sizeof(int), v);
 
     printf("\nAfter:\t");
     print_vet(sizeof(v) / sizeof(int), v);
 
     return 0;
+}
+
+void optimized_bubble_sort(int len, int v[len])
+{
+    for (int i = 0; i < len; i++)
+    {
+        int swapped = 0;
+        for (int j = 0; j < len - 1; j++)
+            if (v[j] > v[j + 1])
+            {
+                swapped = 1;
+                v[j] = v[j] ^ v[j + 1];
+                v[j + 1] = v[j] ^ v[j + 1];
+                v[j] = v[j] ^ v[j + 1];
+            }
+        if (swapped == 0)
+            return;
+    }
 }
 
 void bubble_sort(int len, int v[len])
