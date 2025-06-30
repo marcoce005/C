5199528acd003ff21c434e700aec977006da3f86
marcoce005 <marcoce005@gmail.com>
2025-06-30 17:47:42 +0200

    change in tecniche: change in relazione

--- poli/tecniche_programmazione/esercitazioni/esame/cellini_341045_relazione_esame/cellini_341045_script.c
+++ poli/tecniche_programmazione/esercitazioni/esame/cellini_341045_relazione_esame/cellini_341045_script.c
@@ -25,10 +25,10 @@ int main()
 void raggruppaBambini(bambino b[], int n, int conteggi[10])
 {
     int i, n_attivita;
-    for (i = 0; i < n; i++)
+    for (i = 0; i < n; i++)             // avevo messo len, spiegazione nella relazione
     {
         n_attivita = b[i].n;
-        if (n_attivita > 45)
+        if (n_attivita > 45)               // avevo messo >= [non influenza molto il funzionamento]
             conteggi[9]++;
         else
             conteggi[n_attivita / 5]++;
@@ -37,12 +37,12 @@ void raggruppaBambini(bambino b[], int n, int conteggi[10])
 
 void attivitaPreferite(bambino b[], int n, attivita *preferite[])
 {
-    int i, j, max, n_attivita;
+    int i, j, max, n_attivita;              // avevo dimenticato la virgola tra 'j' e 'max'
     for (i = 0; i < n; i++)
     {
         max = 0;
         n_attivita = b[i].n;
-        for (j = 1; j < n_attivita; j++)
+        for (j = 1; j < n_attivita; j++)            // avevo messo j = 0, facevo una iterazione inutile
             if (b[i].elenco[j].ingressi > b[i].elenco[max].ingressi)
                 max = j;
         preferite[i] = &b[i].elenco[max];
