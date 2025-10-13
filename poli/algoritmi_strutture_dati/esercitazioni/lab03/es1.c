8da3f064418d3e468b7a30aff246ad50a0f1e024
marcoce005 <marcoce005@gmail.com>
2025-10-13 14:28:20 +0200

    add in ASD: update lab03/es1

--- poli/algoritmi_strutture_dati/esercitazioni/lab03/es1.c
+++ poli/algoritmi_strutture_dati/esercitazioni/lab03/es1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int majority(int *a, int N); // wrapper
-int majority_recursive(int *a, int N, int start);
+int majority_recursive(int *a, int N, int pos);
 
 int main(void)
 {
@@ -16,19 +16,19 @@ int main(void)
 
 int majority(int *a, int N) { return majority_recursive(a, N, 0); }
 
-int majority_recursive(int *a, int N, int start)
+int majority_recursive(int *a, int N, int pos)
 {
     int i, occ = 1;
 
-    if (N == 0)
+    if (pos >= N)
         return -1;
 
-    for (i = 1; i < N; i++)
+    for (i = 1; i < N - pos && occ <= N / 2; i++)
         if (*a == a[i])
             occ++;
 
-    if (occ > (N + start) / 2)
+    if (occ > N / 2)
         return *a;
     else
-        return majority_recursive(a + 1, N - 1, start + 1);
+        return majority_recursive(a + 1, N, pos + 1);
 }
