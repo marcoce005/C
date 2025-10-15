b4244c03483408540e742ae63fe9293d030c9842
marcoce005 <marcoce005@gmail.com>
2025-10-15 15:11:16 +0200

    add in ASD: update in lab02/es2

--- poli/algoritmi_strutture_dati/esercitazioni/lab02/es2.c
+++ poli/algoritmi_strutture_dati/esercitazioni/lab02/es2.c
@@ -4,6 +4,7 @@
 #define FILE_PATH "mat.txt"
 
 int **malloc2dR(int *r, int *c);
+void malloc2dP(int ***m, int *r, int *c);
 void free2d(int **m, int r);
 void print_mat(int **m, int r, int c);
 void print_vett(int v[], int len);
@@ -13,6 +14,7 @@ int main(void)
 {
     int **mat, nr, nc, *v_white, *v_black, nw, nb;
 
+    // malloc2dP(&mat, &nr, &nc);
     mat = malloc2dR(&nr, &nc);
 
     if (mat == NULL)
@@ -72,6 +74,40 @@ void free2d(int **m, int r)
     for (int i = 0; i < r; free(m[i]), i++)
         ;
     free(m);
+}
+
+void malloc2dP(int ***m, int *r, int *c)
+{
+    FILE *fp = fopen(FILE_PATH, "r");
+    if (fp == NULL)
+    {
+        printf("File error.");
+        exit(1);
+    }
+
+    fscanf(fp, "%d %d", r, c);
+
+    int **m_tmp = (int **)malloc((*r) * sizeof(int *)),
+        i, j;
+
+    if (m_tmp == NULL)
+    {
+        printf("Memory allocation error.");
+        exit(1);
+    }
+
+    for (i = 0; i < *r; i++)
+    {
+        m_tmp[i] = (int *)malloc((*c) * sizeof(int));
+        if (m_tmp[i] == NULL)
+        {
+            printf("Memory allocation error.");
+            exit(1);
+        }
+        for (j = 0; j < *c; j++)
+            fscanf(fp, "%d", m_tmp[i] + j);
+    }
+    *m = m_tmp;
 }
 
 int **malloc2dR(int *r, int *c)
