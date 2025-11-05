2e4c887a65f28935b705b8015128555f1e221013
marcoce005 <marcoce005@gmail.com>
2025-11-05 15:11:51 +0100

    add in ASD: create data structure in lab05/es2

---
+++ poli/algoritmi_strutture_dati/esercitazioni/lab05/es2/es2.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FILE_PATH_TILES "tiles.txt"
+#define FILE_PATH_BOARD "board.txt"
+
+typedef struct // T1 orizontal, T2 vertical
+{
+    char colore_T1, colore_T2;
+    int valore_T1, valore_T2, id;
+} tile;
+
+tile *get_tiles_from_file(int *n_tiles);
+tile *get_board_from_file(tile *t, int **busy, int *r, int *c);
+tile *init_val(tile *t, int n_t, tile *board, int *busy, int n_b, int *n_val);
+tile rot_tile(tile x);
+int is_in_arr(int *v, int n, int x);
+void print_arr(void *v, int n, size_t size);
+void print_board(tile *v, int r, int c);
+void print_tile(tile x);
+
+int main(void)
+{
+    int n_tiles, r, c, *busy, n_val;
+    tile *tiles = get_tiles_from_file(&n_tiles),
+         *board = get_board_from_file(tiles, &busy, &r, &c),
+         *val = init_val(tiles, n_tiles, board, busy, r * c, &n_val);
+
+    print_arr(busy, r * c, sizeof(int));
+    printf("\n\n");
+    print_arr(tiles, n_tiles, sizeof(tile));
+    printf("\n\n");
+    print_arr(val, n_val, sizeof(tile));
+
+    free(tiles);
+    free(board);
+    free(busy);
+    free(val);
+    return 0;
+}
+
+int is_in_arr(int *v, int n, int x)
+{
+    for (int i = 0; i < n; i++)
+        if (v[i] == x)
+            return 1;
+    return 0;
+}
+
+tile *init_val(tile *t, int n_t, tile *board, int *busy, int n_b, int *n_val)
+{
+    tile *val;
+    int tot_busy, i, index;
+
+    for (tot_busy = i = 0; i < n_b; i++)
+        if (busy[i] != -1)
+            tot_busy++;
+
+    *n_val = (n_t - tot_busy) * 2;
+
+    val = (tile *)malloc(*n_val * sizeof(tile));
+
+    for (index = i = 0; i < n_t; i++)
+        if (!is_in_arr(busy, n_b, t[i].id))
+        {
+            val[index++] = t[i];
+            val[index++] = rot_tile(t[i]);
+        }
+
+    return val;
+}
+
+void print_tile(tile x) { printf("%d:%c %d %c %d \t", x.id, x.colore_T1, x.valore_T1, x.colore_T2, x.valore_T2); }
+
+void print_board(tile *v, int r, int c)
+{
+    int i, j;
+    for (i = 0; i < r; i++)
+    {
+        for (j = 0; j < c; j++)
+            print_tile(v[(i * r) + j]);
+        printf("\n");
+    }
+}
+
+void print_arr(void *v, int n, size_t size)
+{
+    for (int i = 0; i < n * size; i += size)
+    {
+        switch (size)
+        {
+        case sizeof(int):
+            printf("%d ", *((int *)(v + i)));
+            break;
+
+        case sizeof(tile):
+            print_tile(*((tile *)(v + i)));
+            break;
+        }
+    }
+}
+
+tile rot_tile(tile x)
+{
+    tile y;
+    y.id = x.id;
+    y.colore_T1 = x.colore_T2;
+    y.colore_T2 = x.colore_T1;
+    y.valore_T1 = x.valore_T2;
+    y.valore_T2 = x.valore_T1;
+    return y;
+}
+
+tile *get_board_from_file(tile *t, int **busy, int *r, int *c)
+{
+    FILE *fp = fopen(FILE_PATH_BOARD, "r");
+    tile *board;
+    int i, is_rot, index;
+
+    if (fp == NULL)
+    {
+        printf("\nFile error.\n");
+        exit(1);
+    }
+
+    fscanf(fp, "%d %d", r, c);
+
+    board = (tile *)malloc((*r) * (*c) * sizeof(tile));
+    *busy = (int *)malloc((*r) * (*c) * sizeof(int));
+
+    for (i = 0; i < (*r) * (*c); i++)
+    {
+        (*busy)[i] = -1; // init busy to -1
+        fscanf(fp, "%d/%d", &index, &is_rot);
+
+        if (index != -1)
+        {
+            (*busy)[i] = t[index].id;
+            if (is_rot)
+                board[i] = rot_tile(t[index]);
+            else
+                board[i] = t[index];
+        }
+    }
+
+    fclose(fp);
+    return board;
+}
+
+tile *get_tiles_from_file(int *n_tiles)
+{
+    FILE *fp = fopen(FILE_PATH_TILES, "r");
+    tile *t, tmp;
+    int i;
+
+    if (fp == NULL)
+    {
+        printf("\nFile error.\n");
+        exit(1);
+    }
+
+    fscanf(fp, "%d", n_tiles);
+
+    t = (tile *)malloc(*n_tiles * sizeof(tile));
+
+    for (i = 0; i < *n_tiles; fscanf(fp, " %c %d %c %d", &t[i].colore_T1, &t[i].valore_T1, &t[i].colore_T2, &t[i].valore_T2), t[i].id = i, i++)
+        ;
+
+    fclose(fp);
+    return t;
+}
