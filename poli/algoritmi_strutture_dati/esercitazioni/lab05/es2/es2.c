707344943c171d068a1b6ac313ab65f633df1874
marcoce005 <marcoce005@gmail.com>
2025-11-06 19:53:08 +0100

    add in ASD: end lab05/es2

--- poli/algoritmi_strutture_dati/esercitazioni/lab05/es2/es2.c
+++ poli/algoritmi_strutture_dati/esercitazioni/lab05/es2/es2.c
@@ -16,22 +16,30 @@ tile *init_val(tile *t, int n_t, tile *board, int *busy, int *tot_busy, int n_b,
 tile rot_tile(tile x);
 int *create_free_index_tab(int *busy, int free_cell, int r, int c);
 int is_in_arr(int *v, int n, int x);
+int cal_points(tile *board, int r, int c);
 void print_arr(void *v, int n, size_t size);
 void print_board(tile *v, int r, int c);
 void print_tile(tile x);
-void disp(int pos, int n, int k, tile *sol, int r, int c, tile *val, int *mark, int *busy, int *index_tab);
+void disp(int pos, int n, int k, tile *sol, int r, int c, tile *val, int *mark, int *busy, int *index_tab, tile *best, int *max);
+void copy_arr(tile *dest, tile *src, int n);
 
 int main(void)
 {
-    int n_tiles, r, c, *busy, n_busy, n_val, *mark, *free_index_tab;
+    int n_tiles, r, c, *busy, n_busy, n_val, *mark, *free_index_tab, max;
     tile *tiles = get_tiles_from_file(&n_tiles),
          *board = get_board_from_file(tiles, &busy, &r, &c),
-         *val = init_val(tiles, n_tiles, board, busy, &n_busy, r * c, &n_val);
+         *val = init_val(tiles, n_tiles, board, busy, &n_busy, r * c, &n_val),
+         *best = (tile *)malloc(r * c * sizeof(tile));
 
     mark = (int *)calloc(n_val, sizeof(int));
     free_index_tab = create_free_index_tab(busy, (r * c) - n_busy, r, c);
+    max = 0;
 
-    disp(0, n_val, n_tiles - n_busy, board, r, c, val, mark, busy, free_index_tab);
+    disp(0, n_val, n_tiles - n_busy, board, r, c, val, mark, busy, free_index_tab, best, &max);
+
+    printf("\nBest board [format tile: <id>:<coloreT1> <valoreT1> <coloreT2> <valoreT2>]:\n");
+    print_board(best, r, c);
+    printf("Points:\t%d\n", max);
 
     free(tiles);
     free(board);
@@ -39,30 +47,70 @@ int main(void)
     free(val);
     free(mark);
     free(free_index_tab);
+    free(best);
     return 0;
 }
 
-void disp(int pos, int n, int k, tile *sol, int r, int c, tile *val, int *mark, int *busy, int *index_tab)
+void copy_arr(tile *dest, tile *src, int n)
 {
-    int i;
+    for (int i = 0; i < n; dest[i] = src[i], i++)
+        ;
+}
+
+int cal_points(tile *board, int r, int c)
+{
+    int i, j, tot, partial;
+    for (tot = i = 0; i < r * c; i += r + 1)
+    {
+        for (partial = board[(i / r) * r].valore_T1, j = ((i / r) * r) + 1; j < ((i / r) * r) + r; j++) // search in row
+        {
+            if (board[j].colore_T1 != board[j - 1].colore_T1)
+            {
+                partial = 0;
+                break;
+            }
+            partial += board[j].valore_T1;
+        }
+        tot += partial;
+        for (partial = board[i % c].valore_T2, j = (i % c) + c; j <= (r - 1) * c + (i % c); j += c) // search in row
+        {
+            if (board[j].colore_T2 != board[j - c].colore_T2)
+            {
+                partial = 0;
+                break;
+            }
+            partial += board[j].valore_T2;
+        }
+        tot += partial;
+    }
+    return tot;
+}
+
+void disp(int pos, int n, int k, tile *sol, int r, int c, tile *val, int *mark, int *busy, int *index_tab, tile *best, int *max)
+{
+    int i, points;
 
     if (pos >= k)
     {
-        print_board(sol, r, c);
-        print_arr(busy, r * c, sizeof(int));
-        printf("\n\n");
+        points = cal_points(sol, r, c);
+        if (points > *max)
+        {
+            *max = points;
+            copy_arr(best, sol, r * c);
+        }
         return;
     }
 
     for (i = 0; i < n; i++)
     {
-        if (mark[i] == 0)
+        if (mark[i] == 0 && !is_in_arr(busy, index_tab[pos], val[i].id))
         {
             mark[i] = 1;
             sol[index_tab[pos]] = val[i];
             busy[index_tab[pos]] = val[i].id;
-            disp(pos + 1, n, k, sol, r, c, val, mark, busy, index_tab);
+            disp(pos + 1, n, k, sol, r, c, val, mark, busy, index_tab, best, max);
             mark[i] = 0;
+            busy[index_tab[pos]] = -1;
         }
     }
     return;
