2c8666819b2a792a7af1cf22c6c648bf58db4ec6
marcoce005 <marcoce005@gmail.com>
2025-11-12 15:05:22 +0100

    add in ASD: end lab06/es1

--- poli/algoritmi_strutture_dati/esercitazioni/lab06/es1/es1.c
+++ poli/algoritmi_strutture_dati/esercitazioni/lab06/es1/es1.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FILE_PATH "att3.txt"
+#define FILE_PATH "att2.txt"
 
 typedef struct
 {
     int si, fi;
 } att;
 
+void print_arr(int *v, int n);
 void print_arr_att(att *v, int n);
 void print_att(att x, char *prefix, char *suffix);
+void print_sol(att *v, int *index, int i);
 void merge_sort(att *v, int n); // wrapper
 void merge_sort_rec(att *a, att *b, int l, int r);
 void merge_2_way(att *a, att *b, int l, int m, int r);
+void solve_DP(att *v, int n);
 att *get_from_file(int *n);
 att void_att(void);
 int is_void_att(att x);
+int is_compatible(att prec, att curr);
+int max(int *v, int n, int *index_max);
 
 int main(void)
 {
@@ -24,15 +29,75 @@ int main(void)
 
     V = get_from_file(&n_att);
 
-    print_arr_att(V, n_att);
-    printf("\n\n");
+    merge_sort(V, n_att); // sort by fi [when the activity end]
 
-    merge_sort(V, n_att);
-
-    print_arr_att(V, n_att);
+    solve_DP(V, n_att);
 
     free(V);
     return 0;
+}
+
+void print_sol(att *v, int *index, int i)
+{
+    if (i == -1)
+        return;
+    print_sol(v, index, index[i]);
+    print_att(v[i], "", "\n");
+}
+
+int max(int *v, int n, int *index_max)
+{
+    int i, max;
+    for (i = 0, max = v[0], *index_max = 0; i < n; i++)
+        if (v[i] > max)
+        {
+            max = v[i];
+            *index_max = i;
+        }
+    return max;
+}
+
+int is_compatible(att prec, att curr)
+{
+    return curr.si >= prec.fi;
+}
+
+void solve_DP(att *v, int n)
+{
+    int *time = (int *)malloc(n * sizeof(int)),
+        *prec_index = (int *)malloc(n * sizeof(int)),
+        i, j, index_max_time, index_best_time;
+
+    prec_index[0] = -1;
+    time[0] = v[0].fi - v[0].si;
+    index_best_time = 0;
+
+    for (i = 1; i < n; i++)
+    {
+        // search for the first compatilble
+        for (j = i - 1; j >= 0 && !is_compatible(v[j], v[i]); j--)
+            ;
+
+        if (j == -1) // no one is compatible
+        {
+            time[i] = v[i].fi - v[i].si;
+            prec_index[i] = -1;
+        }
+        else
+        {
+            time[i] = v[i].fi - v[i].si + max(time, j + 1, &index_max_time); // sum the best compatible
+            prec_index[i] = index_max_time;
+        }
+
+        if (time[i] > time[index_best_time])
+            index_best_time = i;
+    }
+
+    printf("\nBest duration time:\t%d\nSequence:\n", time[index_best_time]);
+    print_sol(v, prec_index, index_best_time);
+
+    free(time);
+    free(prec_index);
 }
 
 void merge_2_way(att *a, att *b, int l, int m, int r)
@@ -46,7 +111,7 @@ void merge_2_way(att *a, att *b, int l, int m, int r)
             b[k] = a[j++];
         else if (j > r)
             b[k] = a[i++];
-        else if (a[i].si <= a[j].si)
+        else if (a[i].fi <= a[j].fi)
             b[k] = a[i++];
         else
             b[k] = a[j++];
@@ -83,6 +148,12 @@ att void_att(void)
 }
 
 void print_att(att x, char *prefix, char *suffix) { printf("%s%d %d%s", prefix, x.si, x.fi, suffix); }
+
+void print_arr(int *v, int n)
+{
+    for (int i = 0; i < n; printf("%d ", v[i]), i++)
+        ;
+}
 
 void print_arr_att(att *v, int n)
 {
